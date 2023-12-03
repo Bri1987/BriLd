17dc2707a99c1b4f6619e58c07d6a4c3a7d8a88c
Bri1987 <1216754835@qq.com>
2023-12-03 21:35:40 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/BriLd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BriLd"
+  "BriLd.pdb"
+  "CMakeFiles/BriLd.dir/main.c.o"
+  "CMakeFiles/BriLd.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BriLd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
